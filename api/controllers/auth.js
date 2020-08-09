7f91e5deb6d8a7777f89437894326327538e64d0
Madesine <kurila28@gmail.com>
2020-08-09 14:49:53 +0300

    Add login route

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,4 +1,28 @@
+const bcrypt = require("bcryptjs");
+
+const User = require("../database/models/User");
+
 const login = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ errors: [{ msg: "Already exists" }] });
+    }
+
+    user = new User({ name, email, password });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+  } catch (error) {
+    console.error(error.message);
+
+    res.status(500).send("Server error");
+  }
   res.json({ msg: "Hello world" });
 };
 
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
-
 const { login } = require("../controllers/auth");
 
+// @route POST api/auth/login
+// @desc Login user
+// @access Public
 router.post("/login", login);
 
 module.exports = router;
